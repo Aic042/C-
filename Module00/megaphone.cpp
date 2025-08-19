e6be91810a943b8160c46d3f9578b7c5784e5d28
Alvaro Ingunza Curro <aingunza@c1r13s6.42madrid.com>
2025-08-19 12:16:53 +0200

    2025-08-19 12:16:53

--- Module00/megaphone.cpp
+++ Module00/megaphone.cpp
@@ -1,11 +1,31 @@
 #include <iostream> // To use cout
 #include <cctype>	// To use toupper()
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	if (argc == 2)
+	// char *str;
+	char c;
+	char mayuscula;
+	int i = 1;
+	int j = 0;
+	while (i != argc)
 	{
-		
-		return (0);
+		while (argv[i][j])
+		{
+			c = argv[i][j];
+			std::cout << mayuscula << (char)toupper(c);
+			j++;
+		}
+		i++;
+		if (i != argc)
+			std::cout << " ";
+		else
+			std::cout << "\n";
 	}
+	
+	// std::cout << "Uppercaser work please\n";
+	// std::cout << str;
+	// std::cout << "\n";
+	// std::cout << toupper(c);
+	// std::cout << "\n";
 }
