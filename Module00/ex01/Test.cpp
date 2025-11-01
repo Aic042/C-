208b72160cfa394df1e2df0e78225fe18a18e4e9
root <root@DESKTOP-7CAO994.localdomain>
2025-11-01 10:49:29 +0100

    2025-11-01 10:49:29

--- Module00/ex01/Test.cpp
+++ Module00/ex01/Test.cpp
@@ -1,31 +1,31 @@
-#include "lib.h"
+// #include "lib.h"
 
-int main(int argc, char **argv)
-{
-	int edad;
-	std::string name;
-	std::string mode;
-	std::cout << "What mode will you use?\n";
-	std::cin >> mode;
-	if (mode == "edad")
-	{
-		std::cout << "What is your age?\n";
-		std::cin >> edad;
-		std::cout << "You are " << edad << " Years old\n";
-		return 0;
-	}
-	else if (mode == "name")
-	{
-		std::cout << "What is your name?\n";
-		std::cin >> name;
-		std::cout << "Hello " << name << "\n";
-		return 0;
-	}
-	else
-	{
-		std::cout << "Mode selected was not valid\n";
-		return 1;
-	}
-}
+// int main(int argc, char **argv)
+// {
+// 	int edad;
+// 	std::string name;
+// 	std::string mode;
+// 	std::cout << "What mode will you use?\n";
+// 	std::cin >> mode;
+// 	if (mode == "edad")
+// 	{
+// 		std::cout << "What is your age?\n";
+// 		std::cin >> edad;
+// 		std::cout << "You are " << edad << " Years old\n";
+// 		return 0;
+// 	}
+// 	else if (mode == "name")
+// 	{
+// 		std::cout << "What is your name?\n";
+// 		std::cin >> name;
+// 		std::cout << "Hello " << name << "\n";
+// 		return 0;
+// 	}
+// 	else
+// 	{
+// 		std::cout << "Mode selected was not valid\n";
+// 		return 1;
+// 	}
+// }
 
 //cer para error =D
--- Module00/ex01/main.cpp
+++ Module00/ex01/main.cpp
@@ -13,21 +13,22 @@ void add_mode(std::string)
 
 int main(int argc, char **argv)
 {
-	int edad;
+	(void)argc;
+	(void)argv;
 	int num_of_contacts = 0;
 	std::string name;
 	std::string mode;
 	while (1)
 	{
-		// std::cout << "What mode will you use?\n";
-		// std::cin >> mode;
-		pick_mode(mode);
+		std::cout << "What mode will you use?\n";
+		std::cin >> mode;
+		// pick_mode(mode); // Falla 
 		if (mode == "add")
 		{
 			std::cout << "Add the info about this contact\n";
 			std::cin >> name;
 			std::cout << "New contact's name is " << name << "\n";
-			if (num_of_contacts > 8)
+			if (num_of_contacts >= 8)
 			{
 				std::cout << "Too many contacts, exiting now\n";
 				break;
@@ -46,7 +47,7 @@ int main(int argc, char **argv)
 		}
 		else
 		{
-			std::cout << "Mode selected was not valid, please select a valid mode\n";
+			std::cerr << "Mode selected was not valid, please select a valid mode\n";
 		}
 	}
 }
