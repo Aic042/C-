ee4ff262297efcf7fd74bdd9f9409d808c01d895
Aic042 <159058707+Aic042@users.noreply.github.com>
2026-02-16 14:59:07 +0100

    CPP01
    C++++++++ modulo 01

---
+++ CPP01/Zombie.cpp
@@ -0,0 +1,6 @@
+#include "lib.h"
+
+Zombie* newZombie(std :: string name)
+{
+	
+}
---
+++ CPP01/main.cpp
@@ -0,0 +1,13 @@
+#include "lib.h"
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	Zombie* newZombie( std::string name );
+	
+	std::string ZombieName;
+	std::cout << ZombieName << ": BraiiiiiiinnnzzzZ...\n";
+	// delete ;
+	return 0;
+}
