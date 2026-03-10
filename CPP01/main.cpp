36b52c50b31608e8990cd72409cad02b80ceb772
Alvaro Ingunza Curro <aingunza@c2r17s6.42madrid.com>
2026-03-10 12:35:37 +0100

    2026-03-10 12:35:37

--- CPP01/main.cpp
+++ CPP01/main.cpp
@@ -7,7 +7,12 @@ int main(int argc, char **argv)
 	Zombie* newZombie( std::string name );
 	
 	std::string ZombieName;
+	ZombieName = "asdasdasd";
 	std::cout << ZombieName << ": BraiiiiiiinnnzzzZ...\n";
 	// delete ;
+	std::cout << "Now Random Chump:";
+	randoChump(); 
 	return 0;
 }
+
+
--- CPP01/randomChump.cpp
+++ CPP01/randomChump.cpp
@@ -1,1 +1,9 @@
 #include "lib.h"
+
+void randoChump()
+{
+	Zombie* randomChump;
+	std::string ZombieName;
+	ZombieName = "RandomChump";
+	std::cout << ZombieName << rand() << ": BraiiiiiiinnnzzzZ...\n";
+}
