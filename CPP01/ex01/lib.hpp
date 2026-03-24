d9b23151d6abac100c006e15502e9309e23b0e15
root <root@DESKTOP-7CAO994.localdomain>
2026-03-24 07:26:07 +0100

    2026-03-24 07:26:07

---
+++ CPP01/ex01/lib.hpp
@@ -0,0 +1,20 @@
+
+#ifndef HORDE
+# define HORDE
+
+# include <stdio.h>
+# include <iostream> // To use cout
+# include <cctype>	// To use toupper()
+# include <stdio.h>
+
+struct lib
+{
+	private:
+		std::string name;
+	public:
+		
+
+};
+
+
+#endif 
---
+++ CPP01/ex01/main.cpp
@@ -0,0 +1,6 @@
+#include <lib.hpp>
+
+Zombie* zombieHorde( int N, std::string name )
+{
+	
+}
