eeeadb8c43d07e98f501e97cd150f0a515e23249
root <root@DESKTOP-7CAO994.localdomain>
2026-04-25 09:20:36 +0200

    2026-04-25 09:20:36

--- CPP01/ex00/Zombie.cpp
+++ CPP01/ex00/Zombie.cpp
@@ -4,3 +4,10 @@ void Zombie::announce()
 {
     std::cout << this->zombie_name << ": BraiiiiiiinnnzzzZ\n";
 }
+Zombie::Zombie(std::string name){  //Constructor
+    zombie_name = name;
+    std::cout << zombie_name << " Ha sido creado\n";
+}
+Zombie::~Zombie(){  //Destructor
+    std::cout << zombie_name << " ha sido destruido\n";
+}
--- CPP01/ex00/lib.hpp
+++ CPP01/ex00/lib.hpp
@@ -6,29 +6,22 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 10:22:38 by aingunza          #+#    #+#             */
-/*   Updated: 2026/04/22 10:23:20 by root             ###   ########.fr       */
+/*   Updated: 2026/04/25 01:08:25 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BRAINZ
 # define BRAINZ
 
-# include <stdio.h>
 # include <iostream> // To use cout
 # include <cctype>	// To use toupper()
-# include <stdio.h>
 
 class Zombie{
 	private:
 		std::string zombie_name;
 	public:
-		Zombie(std::string name){  //Constructor
-			zombie_name = name;
-			std::cout << zombie_name << " Ha sido creado\n";
-		}
-		~Zombie(){  //Desstructor
-			std::cout << zombie_name << " ha sido destruido\n";
-		}
+		Zombie(std::string name);
+		~Zombie();  //Destructor
 		void announce();
 	};
 Zombie* newZombie( std::string name );
