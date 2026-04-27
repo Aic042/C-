d75bb801ff7534b29bc26069842c5380d78f0f2a
root <root@DESKTOP-7CAO994.localdomain>
2026-04-27 07:48:21 +0200

    2026-04-27 07:48:21

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -1,11 +1,16 @@
 #include "Fixed.hpp"
 
+Fixed::Fixed(Fixed &copied)
+{
+    std::cout << "Copy constructor has been called" << std::endl;
+    copied.fixed_point_num_value = 0;
+}
+
 int Fixed::getRawBits( void ) const
 {
     std::cout << "getRawBits member fucntion called" << std::endl;
     return (this->fixed_point_num_value);
 }
-
 
 void Fixed::setRawBits( int const raw )
 {
